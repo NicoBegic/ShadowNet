57bd7b058cdd06d25995e4b97d6bfebb7fbf0505
DoughnutSlayer <nikolai.elich@gmail.com>
2016-08-23 23:15:33 +0200

    [Added] Functionality for Log Out Button
    [Removed] Placeholder Script

---
+++ Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public void switchToScene()
+    {
+        //SceneManager.LoadScene(sceneName);
+        print("Loading LoginScreen");
+    }
+}
