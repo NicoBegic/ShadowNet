3a925a4de20fefdd200ed1c2d43bc8102da5a89e
DoughnutSlayer <nikolai.elich@gmail.com>
2016-09-05 04:55:42 +0200

    [Added] Inventory.Container Class
    InventoryItem:
    [Changed] Made _weight a public field.
    [Added] private fields for the InputFields.
    [Added] A private method to assign the InputFields.
    [Added] Comments and preconditions.
    [Added] public methods to test the preconditions.
    
    [Changed] Updated Unity to version 5.4.0f3.

---
+++ Assets/Scripts/Inventar/Container.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Inventory
+{
+    public class Container : MonoBehaviour
+    {
+        private string _name;
+
+        private GameObject _containerObject;
+
+        private IList<InventoryItem> _items;
+    }
+}
--- Assets/Scripts/Inventar/InventoryItem.cs
+++ Assets/Scripts/Inventar/InventoryItem.cs
@@ -6,45 +6,99 @@ using System;
 
 namespace Inventory
 {
+    /// <summary>
+    /// InventoryItem represents an item in the player's inventory.
+    /// </summary>
     public class InventoryItem : MonoBehaviour
     {
         private static GameObject InventoryItemPrefab;
 
+        public int _weight; //_weight is in grams.
+
         private string _name, _description;
-        private int _value, _weight; //_value is in cents and _weight is in grams.
+        private int _value; //_value is in cents.
 
         private GameObject _itemObject;
+        private InputField _nameField, _descriptionField, _valueField, _weightField;
 
+        /// <summary>
+        /// Creates a new InventoryItem with blank values.
+        /// </summary>
+        /// <require>InventoryItem.hasPrefab()</require>
         public InventoryItem()
         {
             _itemObject = Instantiate(InventoryItemPrefab);
+            assignInputFields();
             registerListeners();
         }
 
+        public static bool hasPrefab()
+        {
+            return InventoryItemPrefab != null;
+        }
+
+        /// <summary>
+        /// Examines wether a GameObject can be used as a prefab for InventoryItem.
+        /// The GameObject must have four children named "Name", "Beschreibung", "Wert" and "Gewicht", each of which must have an InputField component attached.
+        /// </summary>
+        /// <param name="prefab">The prefab that should be examined.</param>
+        /// <returns>Wether the GameObject is suitable.</returns>
+        public static bool isValidInventoryItemPrefab(GameObject prefab)
+        {
+            if (prefab == null) return false;
+
+            GameObject instance = Instantiate(prefab);
+            ArrayList fieldList = new ArrayList(4);
+
+            fieldList.Add(instance.transform.Find("Name"));
+            fieldList.Add(instance.transform.Find("Beschreibung"));
+            fieldList.Add(instance.transform.Find("Wert"));
+            fieldList.Add(instance.transform.Find("Gewicht"));
+
+            foreach (Transform field in fieldList)
+            {
+                if (field == null || field.GetComponent<InputField>() == null)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        /// <param name="prefab">The prefab that should be used as the GameObject for newly created InventoryItems</param>
+        /// <require>InventoryItem.isValidInventoryItemPrefab(prefab)</require>
         public void setInventoryItemPrefab(GameObject prefab)
         {
             InventoryItemPrefab = prefab;
         }
 
+        private void assignInputFields()
+        {
+            _nameField = _itemObject.transform.Find("Name").GetComponent<InputField>();
+            _descriptionField = _itemObject.transform.Find("Beschreibung").GetComponent<InputField>();
+            _valueField = _itemObject.transform.Find("Wert").GetComponent<InputField>();
+            _weightField = _itemObject.transform.Find("Gewicht").GetComponent<InputField>();
+        }
+
         private void registerListeners()
         {
-            InputField nameField = _itemObject.transform.Find("Name").GetComponent<InputField>();
-            nameField.onEndEdit.AddListener(new UnityAction<string>(delegate (string inputText)
+            _nameField.onEndEdit.AddListener(new UnityAction<string>(delegate (string inputText)
             {
                 _name = inputText;
             }));
-            InputField descriptionField = _itemObject.transform.Find("Beschreibung").GetComponent<InputField>();
-            nameField.onEndEdit.AddListener(new UnityAction<string>(delegate (string inputText)
+
+            _descriptionField.onEndEdit.AddListener(new UnityAction<string>(delegate (string inputText)
             {
                 _description = inputText;
             }));
-            InputField valueField = _itemObject.transform.Find("Name").GetComponent<InputField>();
-            nameField.onEndEdit.AddListener(new UnityAction<string>(delegate (string inputText)
+
+            _valueField.onEndEdit.AddListener(new UnityAction<string>(delegate (string inputText)
             {
                 _value = (int)Math.Round(Convert.ToDecimal(inputText) * 100);
             }));
-            InputField weightField = _itemObject.transform.Find("Name").GetComponent<InputField>();
-            nameField.onEndEdit.AddListener(new UnityAction<string>(delegate (string inputText)
+
+            _weightField.onEndEdit.AddListener(new UnityAction<string>(delegate (string inputText)
             {
                 _weight = (int)Math.Round(Convert.ToDecimal(inputText) * 1000);
             }));
